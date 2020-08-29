0febc81e27d9558630f16d01e90cc22ab7967292
unknown <tommyconner96@gmail.com>
2020-08-28 20:29:59 -0400

    ring buffer done

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,23 @@
 class RingBuffer:
+    # class that implements a new buffer
     def __init__(self, capacity):
-        pass
+        self.data = []
+        self.capacity = capacity
+        # keep track of current node
+        self.cur = 0
 
     def append(self, item):
-        pass
+        # if not at capacity, just append normally
+        if len(self.data) < self.capacity:
+            self.data.append(item)
+        elif self.cur == self.capacity:
+            self.cur = 0
+            self.data[self.cur] = item
+        elif len(self.data) >= self.capacity:
+            self.data[self.cur] = item
+        else:
+            return "an error occured"
+        self.cur += 1
 
     def get(self):
-        pass
+        return self.data
